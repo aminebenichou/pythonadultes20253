f30bb7f0d3a96db8ed3be8f7d3eff5ff8c266dc1
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2025-07-16 12:36:58 +0100

    sync

--- xo2.py
+++ xo2.py
@@ -15,18 +15,27 @@ y_coords = [
     400
 ]
 squares=[
-    {'x_checked':False,'o_checked':False, 'start_coords':(0, 0), 'end_coords':(190, 190)},
-    {'x_checked':False,'o_checked':False, 'start_coords':(200, 0), 'end_coords':(390, 190)},
-    {'x_checked':False,'o_checked':False, 'start_coords':(400, 0), 'end_coords':(590, 190)},
-    {'x_checked':False,'o_checked':False, 'start_coords':(0, 200), 'end_coords':(190, 390)},
-    {'x_checked':False,'o_checked':False, 'start_coords':(200, 200), 'end_coords':(390, 390)},
-    {'x_checked':False,'o_checked':False, 'start_coords':(400, 200), 'end_coords':(590, 390)},
-    {'x_checked':False,'o_checked':False, 'start_coords':(0, 400), 'end_coords':(190, 590)},
-    {'x_checked':False,'o_checked':False, 'start_coords':(200, 400), 'end_coords':(390, 590)},
-    {'x_checked':False,'o_checked':False, 'start_coords':(400, 400), 'end_coords':(590, 590)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(0, 0), 'end_coords':(190, 190)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(200, 0), 'end_coords':(390, 190)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(400, 0), 'end_coords':(590, 190)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(0, 200), 'end_coords':(190, 390)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(200, 200), 'end_coords':(390, 390)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(400, 200), 'end_coords':(590, 390)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(0, 400), 'end_coords':(190, 590)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(200, 400), 'end_coords':(390, 590)},
+    {'x_checked':False, 'o_checked':False, 'start_coords':(400, 400), 'end_coords':(590, 590)},
 ]
 first_checked = False
-
+winning_combos=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
 def draw_x(square):
     pygame.draw.line(screen, 'black', square['start_coords'], square['end_coords'], width=5)
     pygame.draw.line(screen, 'black', (square['end_coords'][0], square['start_coords'][1]), (square['start_coords'][0],square['end_coords'][1]), width=5)
@@ -34,6 +43,14 @@ def draw_x(square):
 def draw_o(square):
     pygame.draw.circle(screen, 'black', (square['start_coords'][0]+square_size/2, square['start_coords'][1]+square_size/2), 50)
     pygame.draw.circle(screen, 'white', (square['start_coords'][0]+square_size/2, square['start_coords'][1]+square_size/2), 40)
+
+def get_winner():
+    for combo in winning_combos:
+        if all(squares[i]['x_checked'] for i in combo):
+            return True
+        elif all(squares[i]['o_checked'] for i in combo):
+            return True
+
 
 turn=0
 
@@ -65,7 +82,8 @@ while running:
             draw_x(square)
         elif square['o_checked']:
             draw_o(square)
-
+    if get_winner():
+        print("You Won")
     pygame.display.flip()
     pygame.display.update()
     clock.tick(60)  
